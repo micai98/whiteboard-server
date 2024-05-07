3a338c852f0a29aa49bbbcdd292801471d2e7058
micai <47612411+micai98@users.noreply.github.com>
2024-05-07 21:34:36 +0200

    added voting to clear the canvas

--- wbserver/__main__.py
+++ wbserver/__main__.py
@@ -20,14 +20,21 @@ userdict = {}
 roomdict = {}
 codedict = {}
 
-def gen_room_code(min_length=4):
+usercount = 0
+clearvotes = 0
+
+# generates a random unique room code
+def gen_room_code(min_length:int=4):
+    """generates a random unique room code"""
     code = ''
     lastchar = len(ROOMCODE_CHARS)-1
     while len(code) < min_length or code in roomdict:
         code += ROOMCODE_CHARS[random.randint(0, lastchar)]
     return code
 
-def print_all(text, type=MSG_SYSINFO):
+# sends a chat message to all connected users
+def chat_all(text:str, type:int=MSG_SYSINFO):
+    """sends a chat message to all connected users"""
     msgdict = {
         "timestamp": 0,
         "variant": type,
@@ -38,42 +45,66 @@ def print_all(text, type=MSG_SYSINFO):
 
 @sio.event
 def connect(sid, environ, auth):
+    global usercount, clearvotes
+    usercount += 1
     print('> connect: ', sid, " auth: ", auth)
-    print_all(f"joining: {sid}", MSG_USERJOIN);
+    chat_all(f"joining: {sid}", MSG_USERJOIN);
 
+@sio.event
+def disconnect(sid):
+    global usercount, clearvotes
+    usercount -= 1
+    clearvotes = 0
+    print(f'> disconnect: (socket: {sid})')
+    chat_all(f"leaving: {sid}", MSG_USERLEAVE);
+
+# test event
 @sio.event
 def ping(sid):
     print(f'pong! {sid}')
 
+# sent by clients when they're ready to have the canvas sent to them
 @sio.event
 def client_ready(sid):
     print(f"client ready: {sid}")
     sio.emit("canvas_request_state", skip_sid=sid)
 
+# sent by clients as a response to canvas_request_state. forwards the state of the canvas to other clients in order to synchronize them
 @sio.event
 def canvas_state(sid, data):
     sio.emit("canvas_receive_state", data)
 
+# drawing
 @sio.event
 def draw_line(sid, data):
     print(sid, data)
     sio.emit("draw_line", data=data, skip_sid=sid)
 
+# voting to clear canvas
+@sio.event
+def vote_clear(sid):
+    global clearvotes, usercount
+    clearvotes += 1
+    chat_all(f"{sid} voted to clear the canvas ({clearvotes}/{usercount})")
+    if(clearvotes >= usercount):
+        chat_all("Cleared the canvas")
+        sio.emit("canvas_clear")
+
+# receive user's attempt to send a message, then validate and broadcast it
 @sio.event
 def msg_send(sid, data):
     print(sid, data)
-    msgdict = {
+    msg = {
         "timestamp": 0,
         "variant": MSG_USERMSG,
         "user": sid,
         "content": data
     }
-    sio.emit("msg_broadcast", msgdict)
-
-@sio.event
-def disconnect(sid):
-    print_all(f"leaving: {sid}", MSG_USERLEAVE);
-    print(f'> disconnect: (socket: {sid})')
+    if len(data) > 0:
+        sio.emit("msg_broadcast", msg)
+    else:
+        print(f"> ignoring empty message attempt ({sid})")
+    
 
 if __name__ == '__main__':
     frame(f" Starting server \r sv_host = {sv_host} \n sv_port = {sv_port} ")
